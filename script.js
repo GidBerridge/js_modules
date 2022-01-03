3ddde830ef88e6a2632c31817708c299a7572396
Gid Baerridge <gid.berridge@gmail.com>
2022-01-03 11:44:25 +0000

    all

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // importing module
-import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
+// import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 console.log('Importing module');
 
-import * as ShoppingCart from "./shoppingCart.js";
+// import * as ShoppingCart from "./shoppingCart.js";
 
 // ShoppingCart.addToCart('bread', 5)
 // console.log(ShoppingCart.totalPrice);
@@ -11,12 +11,12 @@ import * as ShoppingCart from "./shoppingCart.js";
 // console.log(price, tq);
 
 
-// import add, { cart } from "./shoppingCart.js";
-// add('pizza', 2);
-// add('bread', 2);
-// add('apples', 2);
+import add, { cart } from "./shoppingCart.js";
+add('pizza', 5);
+add('bread', 2);
+add('apples', 1);
 
-// console.log(cart);
+console.log(cart);
 
 // console.log('Start fetching');
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts')
@@ -39,28 +39,53 @@ import * as ShoppingCart from "./shoppingCart.js";
 // const lastPost2 = await getLastPost();
 // console.log(lastPost2);
 
-const shoppingCart2 = (function() {
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
+// const shoppingCart2 = (function() {
+//     const cart = [];
+//     const shippingCost = 10;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
 
-    const addToCart = function(product, quantity) {
-    cart.push({ product, quantity});
-    console.log(`${quantity} ${product} added to cart`);
-    };
-    const orderStock = function(product, quantity) {
-    cart.push({ product, quantity});
-    console.log(`${quantity} ${product} ordered from supllier`);
-    };
+//     const addToCart = function(product, quantity) {
+//     cart.push({ product, quantity});
+//     console.log(`${quantity} ${product} added to cart`);
+//     };
+//     const orderStock = function(product, quantity) {
+//     cart.push({ product, quantity});
+//     console.log(`${quantity} ${product} ordered from supllier`);
+//     };
 
-    return {
-        addToCart,
-        cart,
-        totalPrice,
-        totalQuantity,
-    }
-})();
+//     return {
+//         addToCart,
+//         cart,
+//         totalPrice,
+//         totalQuantity,
+//     }
+// })();
 
-shoppingCart2.addToCart('apples', 4);
-shoppingCart2.addToCart('pizza', 2);
+// shoppingCart2.addToCart('apples', 4);
+// shoppingCart2.addToCart('pizza', 2);
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash';
+
+const state = {
+    cart: [
+        {product: 'bread', quantity: 5},
+        {product: 'pizza', quantity: 2},
+    ],
+    user: {loggedIn: true},
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+// console.log(stateClone);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
+
+if (module.hot) {
+    module.hot.accept()
+}
+
+import 'core-js/stable';
